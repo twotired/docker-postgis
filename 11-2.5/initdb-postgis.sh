18e0338cfbc2d488584b41c9fc5497a62b988061
Jason Wells <spinmaster@gmail.com>
2019-02-02 12:19:27 -0800

    adding plpython and happybase

--- 11-2.5/initdb-postgis.sh
+++ 11-2.5/initdb-postgis.sh
@@ -19,5 +19,7 @@ for DB in template_postgis "$POSTGRES_DB"; do
 		CREATE EXTENSION IF NOT EXISTS postgis_topology;
 		CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
 		CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+		create extension plpythonu;
+		create extension plpython2u;
 EOSQL
 done
